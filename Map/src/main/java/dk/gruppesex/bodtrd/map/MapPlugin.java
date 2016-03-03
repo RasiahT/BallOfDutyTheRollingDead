be811ecefc9facd118aaaf8d4aafad8d40a1c7f9
LuqJensen <lucas__jensen@hotmail.com>
2016-03-03 18:33:34 +0100

    Initial Commit
    Added the empty components Common, GameEngine, Input, KeyboardInput, MouseInput, Map, Player, MenuState, and PlayState.
    Added initial dependencies on SPIs.
    Adeed loaders for all components for all but Common and Input.
    Added .gitignore.

---
+++ Map/src/main/java/dk/gruppesex/bodtrd/map/MapPlugin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.gruppesex.bodtrd.map;
+
+import dk.gruppesex.bodtrd.common.services.GamePluginSPI;
+
+/**
+ *
+ * @author lucas
+ */
+public class MapPlugin implements GamePluginSPI
+{
+    @Override
+    public void start()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stop()
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
