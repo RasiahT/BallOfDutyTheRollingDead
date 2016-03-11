8e2633520b514f138071f06dbb1c338ffd6ece30
Ploug <frederik@ploug-johansen.dk>
2016-03-11 14:35:51 +0100

    Engine foundation

--- Common/src/main/java/dk/gruppesex/bodtrd/common/data/GameState.java
+++ Common/src/main/java/dk/gruppesex/bodtrd/common/data/GameState.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dk.gruppesex.bodtrd.common.services;
+package dk.gruppesex.bodtrd.common.data;
 
 /**
  *
- * @author lucas
+ * @author frede
  */
-public interface GameStateSPI
+public enum GameState
 {
-
+    PLAY, PAUSE_MENU, MAIN_MENU, SHOP_MENU
 }
