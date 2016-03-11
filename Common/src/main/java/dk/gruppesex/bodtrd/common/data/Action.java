7c4425533e1d4eaf8ce6290991304ed1efb0ba18
ChrisDaBang <namenotvalid123@gmail.com>
2016-03-11 13:49:44 +0100

    Further implementation of the Player module

---
+++ Common/src/main/java/dk/gruppesex/bodtrd/common/data/Action.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.gruppesex.bodtrd.common.data;
+
+/**
+ *
+ * @author Chris
+ */
+public enum Action
+{
+    MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, SHOOT, RELOAD, PAUSE_MENU;
+
+}
