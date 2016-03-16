ee9e8e8db01257167bb5057e6f99c32fe24a666d
dzenih <dzenita@outlook.dk>
2016-03-16 13:03:27 +0100

    Implemented weapon and shooting..
    There is still some TODO's

---
+++ Weapon/src/main/java/dk/gruppesex/weapon/Installer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.gruppesex.weapon;
+
+import dk.gruppesex.bodtrd.common.services.GamePluginSPI;
+import org.openide.modules.ModuleInstall;
+
+public class Installer extends ModuleInstall
+{
+
+    public static GamePluginSPI Plugin = null;
+
+    @Override
+    public void restored()
+    {
+        // TODO
+    }
+
+    @Override
+    public void uninstalled()
+    {
+        if (Plugin != null)
+        {
+            Plugin.stop();
+        }
+    }
+
+}
